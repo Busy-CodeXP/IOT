cb05f8c4bf3f360b3688ad721613da13378fd0ac
caio132 <caiosouza1106@hotmail.com>
2018-03-08 21:04:53 -0300

    08-03

--- Sensor.js
+++ Sensor.js
@@ -9,6 +9,10 @@ var saida = 0;
 var ObstaculoEntrada = false;
 var ObstaculoSaida = false;
 var final;
+const configuracao = require("./modulo-api");
+const id = configuracao.id;
+const api = configuracao.api;
+const UpdateSensor = apt + "/api/Sensor/" + id;
 
 board.on("ready", function () {
     var SensorEntrada = new five.Proximity({
@@ -22,7 +26,6 @@ board.on("ready", function () {
         freq: 1000
     })
 
-
     SensorEntrada.on("data", function () {
 
         var ObstaculoEntrando = this.cm < 15;
@@ -35,7 +38,10 @@ board.on("ready", function () {
 
         console.log("Número de pessoas que entraram :");
         console.log(entrar);
-    })
+        const DadosEntrada = {
+            valor: entrar
+        };
+    });
 
     SensorSaida.on("data", function () {
 
@@ -48,16 +54,52 @@ board.on("ready", function () {
         ObstaculoSaida = ObstaculoSaindo;
         console.log("Número de pessoas que sairam: ");
         console.log(saida);
+        const DadosSaida = {
+            valor: saida
+        };
 
         final = (entrar - saida);
         if (final <= 0) {
             saida = 0;
             entrar = 0;
         }
-        
-        
         console.log("Número de pessoas que estão no busão: ");
         console.log(final);
+        const dados = {
+            valor: final
 
-    })
-})
+        };
+
+        request.put(UpdateSensor, {
+            json: true,
+            body: {
+                "entradas": DadosEntrada,
+                "saidas": DadosSaida,
+                "total": dados
+            }
+        }, function (erros, res, body) {
+            if (error) {
+                consolo.error(error);
+                return;
+            }
+        });
+
+    });
+
+    setInterval(function () {
+        request.put(UpdateSensor, {
+            json: true,
+            body: {
+                "entradas": DadosEntrada,
+                "saidas": DadosSaida,
+                "total": dados
+            }
+        }, function (erros, res, body) {
+            if (error) {
+                consolo.error(error);
+                return;
+            }
+        });
+    }, 5000);
+
+});
